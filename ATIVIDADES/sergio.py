f7ceda52c2b054394dd11412b83e91172b6fe291
frclasso <frclasso@yahoo.com.br>
2018-09-28 11:51:25 -0300

    pingtool.py

---
+++ ATIVIDADES/sergio.py
@@ -0,0 +1,8 @@
+t = ([0, 7, 8, 9], 1, 2, 3, 4, 5)
+print(t) # Imprime a tupla.
+print(t[0]) # Imprime o item na posição 0 da Tupla, que é uma lista.
+t[0].append(10)  # Acrescenta o número 10 dentro da lista na posição 0 da tupla.
+print(t) # Imprime a tupla.
+print(t[0]) # Imprime o item na posição 0 da Tupla, que é uma lista.
+
+#Sérgio Luiz de Oliveira

bdad08c660f0e98e3479491c72129658d9ef43ad
leandro-beppler <43305302+leandro-beppler@users.noreply.github.com>
2018-10-09 23:00:01 -0300

    Atualização de atividades

---
+++ Leandro Beppler/cadastro_pf_leandro.py
@@ -0,0 +1,102 @@
+import sqlite3, time
+from datetime import datetime
+
+
+connect=sqlite3.connect('pessoafisica.db')
+c=connect.cursor()
+tempo=datetime.now()
+resp = 'S' or 's'
+
+def criar_db():
+    c.execute('CREATE TABLE IF NOT EXISTS cadastro (nome text, cpf varchar, endereco text, email text, empresa text, cargo text)')
+try:
+    criar_db()
+except:
+    print('Erro ao criar banco de dados.')
+else:
+    print('Banco de dados criado com sucesso.')
+
+def inserirDados(n, cpf, end, e, emp, carg):
+    c.execute('INSERT INTO cadastro VALUES(?, ?, ?, ?, ?, ?)', (n, cpf, end, e, emp, carg))
+    connect.commit()
+
+def pesquisaDados(pesquisarDados_db):
+    sql_pesquisa='SELECT * FROM cadastro WHERE nome = ?'
+    for row in c.execute(sql_pesquisa, (pesquisarDados_db,)):
+        print(row)
+    connect.commit()
+
+def relatarCadastros():
+    sql_relatar='SELECT * FROM cadastro'
+    for row in c.execute(sql_relatar):
+        print(row)
+    connect.commit()
+
+def deletarCadastro(deletarCadastro_db):
+    sql_deletar='DELETE FROM cadastro WHERE nome = ?'
+    for row in c.execute(sql_deletar, (deletarCadastro_db,)):
+        print(row)
+    connect.commit()
+
+# def retornaMenu():
+#    if resp == 'S' or 's':
+#
+#    elif 'n' or 'N':
+#       sys.exit()
+#    else:
+#       print('Comando não reconhecido.')
+
+
+# while resp == 'S':
+#     for n in range(1, 11):
+#         print(f'{num}  x {n:2} = {num*n:2}')
+#     print('#*' * 10)
+#     resp = str(input('\nQuer continuar (S/N)? ')).upper()
+#     if resp == 'S':
+#         num = int(input('Digite um número inteiro para calcular a tabuada: '))
+#
+# print('Obrigado por utilizar o meu programa!.')
+
+funcao=int(input('1 - Cadastrar Pessoa Física\n2 - Consultar Cadastro PF\n3 - Relatório de Cadastros\n4 - Deletar Cadastro\nSelecione a opção desejada:'))
+
+if funcao == 1:
+    try:
+        print('Cadastrar Pessoa Física - JCAVI Treinamentos')
+        time.sleep(1)
+        n=str(input('Digite o nome: '))
+        cpf=str(input('Digite o CPF: '))
+        end=str(input('Digite o endereço: '))
+        e=str(input('Digite o email: '))
+        emp=str(input('Digite o nome da empresa: '))
+        carg=str(input('Digite o seu cargo: '))
+        inserirDados(n, cpf, end, e, emp, carg)
+    except:
+        print('\nErro ao realizar cadastro do cliente')
+    else:
+        print('\n{}, foi cadastrado com sucesso.'.format(n))
+              
+if funcao == 2:
+    try:
+        print('Consultar dados no Database...')
+        pesquisardb=str(input('Digite o nome a ser localizado: '))
+        pesquisaDados(pesquisardb)
+        print('\nPesquisa realizada em {}'.format(tempo.strftime('%d/%m/%Y - %H:%M:%S')))
+    except:
+        print('\nOcorreu um erro durante a pesquisa.')
+
+if funcao == 3:
+    try:
+        print('Relatório de usuários:\n')
+        relatarCadastros()
+        print('\nPesquisa finalizada às {}'.format(tempo.strftime('%H:%M:%S - %d/%m/%Y')))
+        #retornaMenu()
+    except:
+        print('\nOcorreu um erro, não foi possível localizar os cadastros.')
+
+if funcao == 4:
+    try:
+        deletarCadastro_db=str(input('Digite o nome a ser removido:'))
+        deletarCadastro(deletarCadastro_db)
+        print('\n{}, foi removido com sucesso.'.format(deletarCadastro_db))
+    except:
+        print('\n{} não foi localizado no cadastro.'.format(deletarCadastro_db))
---
+++ Leandro Beppler/teste_tk.py
@@ -0,0 +1,101 @@
+from tkinter import *
+
+class calculadora_JCAVI:
+    def __init__(self, calc):
+        self.calc = calc
+        self.calc.title("Calculadora - JCAVI")
+        self.calc.geometry('187x177')
+
+        self.valores = Entry(self.calc, bg='Gray', font=('Tahoma', 22))
+        self.valores.grid(row=0,sticky=W, ipadx=93, ipady=9, pady=3)
+
+        self.sete = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.sete['text'] = '7'
+        self.sete.grid(row=1, column=0, sticky=W, ipadx=15, pady=3)
+
+        self.quatro = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.quatro['text'] = '4'
+        self.quatro.grid(row=2, column=0, sticky=W, ipadx=15, pady=3)
+
+        self.um = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.um['text'] = '1'
+        self.um.grid(row=3, column=0, sticky=W, ipadx=15, pady=3)
+
+        self.zero = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.zero['text'] = '0'
+        self.zero.grid(row=4, sticky=W, ipadx=39, pady=3, padx=0)
+
+        self.oito = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.oito['text'] = '8'
+        self.oito.grid(row=1, sticky=W, ipadx=15, pady=2, padx=49)
+
+        self.cinco = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.cinco['text'] = '5'
+        self.cinco.grid(row=2,sticky=W, ipadx=15, pady=3, padx=49)
+
+        self.dois = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.dois['text'] = '2'
+        self.dois.grid(row=3, sticky=W, ipadx=15, pady=3, padx=49)
+
+        self.nove = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.nove['text'] = '9'
+        self.nove.grid(row=1, sticky=W, ipadx=15, pady=2, padx=98)
+
+        self.seis = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.seis['text'] = '6'
+        self.seis.grid(row=2,sticky=W, ipadx=15, pady=3, padx=98)
+
+        self.tres = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.tres['text'] = '3'
+        self.tres.grid(row=3,sticky=W, ipadx=15, pady=3, padx=98)
+
+        self.igual = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.igual['text'] = '='
+        self.igual.grid(row=4, sticky=W, ipadx=14, pady=3, padx=98)
+
+        self.adicao = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.adicao['text'] = '+'
+        self.adicao.grid(row=1,sticky=W, ipadx=12, padx=146)
+
+        self.sub = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.sub['text'] = '-'
+        self.sub.grid(row=2,sticky=W, ipadx=14, padx=146)
+
+        self.mul = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.mul['text'] = '*'
+        self.mul.grid(row=3,sticky=W, ipadx=13, padx=146)
+
+        self.divi = Button(self.calc, relief=GROOVE, activebackground='Red', activeforeground='White')
+        self.divi['text'] = '÷'
+        self.divi.grid(row=4,sticky=W, ipadx=12, padx=146)
+
+calc = Tk()
+calculadora_JCAVI(calc)
+calc.mainloop()
+
+
+
+
+# label1=Label(calculadora, text="Calculadora\nJCAVI Treinamentos")
+# label1.pack(side=TOP)
+#
+# button1 = Entry(calculadora,)
+# button1.pack(side=TOP)
+#
+# seteButton=Button(calculadora, font=('Tahoma', 12), text='7')
+# seteButton.pack(side=LEFT)
+#
+# quatroButton=Button(calculadora, font=('Tahoma', 12), text='4')
+# quatroButton.pack(side=LEFT)
+#
+# umButton=Button(calculadora, font=('Tahoma', 12), text='1')
+# umButton.pack(side=LEFT,)
+#
+# oitoButton=Button(calculadora, font=('Tahoma', 12), text='8')
+# oitoButton.pack(side=RIGHT)
+#
+# cincoButton=Button(calculadora, font=('Tahoma', 12), text='5')
+# cincoButton.pack(side=RIGHT)
+#
+# doisButton=Button(calculadora, font=('Tahoma', 12), text='2')
+# doisButton.pack(side=RIGHT)
